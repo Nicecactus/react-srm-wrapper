4f9193afdc615ccf11e0932b95f3ff415933bd7e
Robin Goupil <robin.goupil@nicecactus.gg>
2021-05-19 10:26:16 +0200

    Updates

---
+++ precache-manifest.55e93ae44db00b38d8a117f79284bea7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d20fda2670a9f7c77ba4057a271bef2f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "153743d89c65e384a596",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "1a70de7fcdf11ff3c0e3",
+    "url": "./static/js/2.f1b50386.chunk.js"
+  },
+  {
+    "revision": "c346956c1449420dcbe7f2a51cdfc9c4",
+    "url": "./static/js/2.f1b50386.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "153743d89c65e384a596",
+    "url": "./static/js/main.c4cf0001.chunk.js"
+  },
+  {
+    "revision": "6a2e5b2d3a75392b31a0",
+    "url": "./static/js/runtime-main.57490fd7.js"
+  }
+]);
